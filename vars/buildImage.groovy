7ea1a0c824b1967b7a5640a42a0e6a2c73880375
Awais Qamar <awaisqamar@Awaiss-MacBook-Air.local>
2025-08-01 17:05:08 +0500

    initial commit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 
 def call(){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: '9b424603-aa50-4504-9c28-a0a86f8f3713', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'github-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t \n' +
                 'qamarha28812/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
